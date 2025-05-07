7b4c716c3b80323347fcacd39f5a94315228e70d
caiosntos <caiogiovanists@outlook.com>
2025-05-07 20:49:51 -0300

    feat: Estudos sobre API Rest

--- API_Rest/index.js
+++ API_Rest/index.js
@@ -9,9 +9,22 @@ app.get('/ufs', (req, res) => {
 
 app.get('/ufs/:iduf', (req, res) => {
     const idUF = parseInt(req.params.iduf);
-    const uf = colecaoUf.find (u => u.id === idUF);
+    let uf;
+    let mensagemErro = '';
 
+    if (!(isNaN(idUF))){
+        uf = colecaoUf.find (u => u.id === idUF);
+        if(!uf){
+            mensagemErro = 'UF não encontrada';
+        }else{
+            mensagemErro = 'Requisição Inválida';
+        }
+    }
+    if(uf){
     res.json(uf);
+    }else{
+        res.status(404).send({"erro": mensagemErro});
+    }
 });
 
 app.listen(8080, () =>{
